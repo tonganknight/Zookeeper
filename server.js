6506e699b14a024ec3ac8fa45914ff7e2b70bae5
Malachi <malachialusa@gmail.com>
2021-01-26 15:40:35 -0700

    Added work for server errors

--- server.js
+++ server.js
@@ -53,9 +53,23 @@ function filterByQuery(query, animalsArray) {
     res.json(results);
   });
 
+  function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+  }
+
+  app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+      res.json(result);
+    } else {
+      res.send(404);
+    }
+  });
+  
+  
 
 
-
-app.listen(PORT, () => {
+  app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
   });
